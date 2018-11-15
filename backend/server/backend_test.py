aa99784303c8d28a9825269f154acdb8a32d3dd6
jugeeya <jugeeya@live.com>
2018-11-15 12:19:19 -0800

    Edit basic tests to function properly with cleanup

--- backend/server/backend_test.py
+++ backend/server/backend_test.py
@@ -10,7 +10,7 @@ password = 'example';
 url = "mongodb://{}:{}@{}".format(username, password, uri);
 client = MongoClient(url)
 
-db = client['gobble']
+db = client.admin
 
 class TestUserMethods(unittest.TestCase):
 
@@ -21,14 +21,22 @@ class TestUserMethods(unittest.TestCase):
                 }
         r = requests.post(endpoint+"/register", data)
         self.assertEqual(r.status_code, 200)
+        db.users.remove({})
 
     def test_getUsers(self):
+        data = {'Name': 'testUser1',
+                'Password': 'somePassword',
+                'PasswordConf': 'somePassword',
+                }
+        r = requests.post(endpoint+"/register", data)
+        self.assertEqual(r.status_code, 200)
         r = requests.get(endpoint+"/users")
         foundUser = False
         for user in r.json():
-            if user.name == 'testUser1':
+            if user['name'] == 'testUser1':
                 foundUser = True
         self.assertTrue(foundUser)
+        db.users.remove({})
 
 if __name__ == '__main__':
     unittest.main()
