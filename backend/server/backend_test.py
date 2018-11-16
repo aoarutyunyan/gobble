823c7a33ae8433cfd34241fd09235e9a538f825a
jugeeya <jugeeya@live.com>
2018-11-15 18:22:09 -0800

    Improve backend test infrastructure

--- backend/server/backend_test.py
+++ backend/server/backend_test.py
@@ -12,31 +12,37 @@ client = MongoClient(url)
 
 db = client.admin
 
+def registerUser(registerData):
+    return requests.post(endpoint+"/register", registerData)
+
+def getUsers():
+    return requests.get(endpoint+"/users").json()
+
+def removeUser(userData):
+    for user in db.users.find(userData):
+        db.users.remove(user['_id'])
+
+regData1 = {'Name': 'testUser1',
+        'Password': 'somePassword',
+        'PasswordConf': 'somePassword'}
+userData1 = {'name': 'testUser1'}
+
 class TestUserMethods(unittest.TestCase):
 
     def test_register(self):
-        data = {'Name': 'testUser1',
-                'Password': 'somePassword',
-                'PasswordConf': 'somePassword',
-                }
-        r = requests.post(endpoint+"/register", data)
+        r = requests.post(endpoint+"/register", regData1)
         self.assertEqual(r.status_code, 200)
-        db.users.remove({})
+        removeUser(userData1)
 
     def test_getUsers(self):
-        data = {'Name': 'testUser1',
-                'Password': 'somePassword',
-                'PasswordConf': 'somePassword',
-                }
-        r = requests.post(endpoint+"/register", data)
+        r = registerUser(regData1)
         self.assertEqual(r.status_code, 200)
-        r = requests.get(endpoint+"/users")
         foundUser = False
-        for user in r.json():
+        for user in getUsers():
             if user['name'] == 'testUser1':
                 foundUser = True
         self.assertTrue(foundUser)
-        db.users.remove({})
+        removeUser(userData1)
 
 if __name__ == '__main__':
     unittest.main()
