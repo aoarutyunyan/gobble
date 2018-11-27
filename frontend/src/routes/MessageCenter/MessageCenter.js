2dc3f2170931cea1ca8b51601207a4d84a75887b
Arthur Arutyunyan <aoarutyunyan@gmail.com>
2018-11-27 03:30:59 -0800

    Removed unnecessary styling and file name change

--- frontend/src/routes/MessageCenter/MessageCenter.js
+++ frontend/src/routes/MessageCenter/MessageCenter.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Content = styled.div`
-  margin-top: 2em;
-  display: grid;
-  grid-template-columns: 130px 1fr;
-  padding: 0 40px;
+const Wrapper = styled.div`
+
 `;
 
 export const MessageCenter = () => {
   return (
-    <Content>
-    </Content>
+    <Wrapper>
+
+    </Wrapper>
   );
 };
 
