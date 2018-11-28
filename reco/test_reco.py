967374ddc65de26fb46fbd35621ae8c69f4e654a
kevinnnyi <36563011+kevinnnyi@users.noreply.github.com>
2018-11-27 18:10:19 -0800

    added personal comments, unit test class

--- reco/test_reco.py
+++ reco/test_reco.py
@@ -1,11 +1,27 @@
 import pytest
 from reco import ChefRecommendationEngine
 
+# unit testing
+class TestUnit():
+    def test_compute_one(self):
+        pass
 
+    def test_compute_two(self):
+        pass
+
+'''
+normal reco control flow:
+1. given dataset of {user_id, chef_id, rating}
+2. compute similarity matrix
+3. find top K similar users for each user
+4. from these similar users, return list top N rated chef_ids
+'''
+
+# functional testing
 class TestReco():
     def test_one(self):
-        x = ChefRecommendationEngine()
-        similar = x.get_top_similar_users(20, 2)
-        top = x.get_top_recs(20, similar, 2)
+        reco = ChefRecommendationEngine()
+        similar = reco.get_top_similar_users(20, 2)
+        top = reco.get_top_recs(20, similar, 2)
 
         print(top)
