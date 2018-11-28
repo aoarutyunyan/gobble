79ad9a5f3fe534835298fa14e9923a5825d9aa15
kevinnnyi <36563011+kevinnnyi@users.noreply.github.com>
2018-11-27 21:56:20 -0800

    chose some tests

--- reco/test_reco.py
+++ reco/test_reco.py
@@ -1,13 +1,6 @@
 import pytest
+import pytest_cov
 from reco import ChefRecommendationEngine
-
-# unit testing
-class TestUnit():
-    def test_compute_one(self):
-        pass
-
-    def test_compute_two(self):
-        pass
 
 '''
 normal reco control flow:
@@ -16,12 +9,17 @@ normal reco control flow:
 3. find top K similar users for each user
 4. from these similar users, return list top N rated chef_ids
 '''
-
-# functional testing
 class TestReco():
-    def test_one(self):
-        reco = ChefRecommendationEngine()
-        similar = reco.get_top_similar_users(20, 2)
-        top = reco.get_top_recs(20, similar, 2)
+    def test_initialization(self):
+        reco = ChefRecommendationEngine("./testratings.csv")
+        # compute cosine similarity matrices, assert that they're correct
 
-        print(top)
+    # gonna add new rating with new user, assert that matrix changes correctly
+    def test_recomputation(self):
+        pass
+
+    def test_top_users(self):
+        pass
+
+    def test_top_recs(self):
+        pass
